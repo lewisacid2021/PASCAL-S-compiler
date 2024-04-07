8de4d4141e13275c04ff7196c50fc1b3a86060a0
Kaletsu <1114829092@qq.com>
2024-04-06 21:07:13 -0700

    add semanticAnalysis.cpp and updata symbolTable.h and update symbolTable.cpp

--- include/symbolTable.h
+++ include/symbolTable.h
@@ -19,18 +19,18 @@ class TableRecord
     bool isMinus;                       //常量前是否有负号
     int amount;                         //参数个数/数组维数/字符串字符数
     vector<pair<int, int>> arrayRange;  //数组各维上下界
-    SymbolTable* subSymbolTable;        //指向函数/过程子符号表的指针
+    SymbolTable *subSymbolTable;        //指向函数/过程子符号表的指针
     string programInfo;                 //当前符号表对应的程序相关信息
 
-    void setPara(string id, int rowNumber, string type);
-    void setVarPara(string id, int rowNumber, string type);
-    void setVar(string id, int rowNumber, string type);
-    void setConst(string id, int rowNumber, string type, bool isMinus, string value);
-    void setArray(string id, int rowNumber, string type, int amount, vector<pair<int, int>> arrayRange);
-    void setProcedure(string id, int rowNumber, int amount, SymbolTable *subSymbolTable);
-    void setFunction(string id, int rowNumber, string type, int amount, SymbolTable *subSymbolTable);
-    void setProgramName(string id, int rowNumber, string programInfo, int amount, string returnType);
-    void setVoidPara(string id, int rowNumber);
+    void setPara(string id_para, int rowNumber_para, string type_para);
+    void setVarPara(string id_para, int rowNumber_para, string type_para);
+    void setVar(string id_para, int rowNumber_para, string type_para);
+    void setConst(string id_para, int rowNumber_para, string type_para, bool isMinus_para, string value_para);
+    void setArray(string id_para, int rowNumber_para, string type_para, int amount_para, vector<pair<int, int>> arrayRange_para);
+    void setProcedure(string id_para, int rowNumber_para, int amount_para, SymbolTable *subSymbolTable);
+    void setFunction(string id_para, int rowNumber_para, string type_para, int amount_para, SymbolTable *subSymbolTable);
+    void setProgramName(string id_para, int rowNumber_para, string programInfo_para, int amount_para, string returnType_para);
+    void setVoidPara(string id_para, int rowNumber_para);
 
     string findXthFormalParaType(int X);             //找到第X个形式参数的类型
     bool isXthFormalParaRefered(int X);              //检查第X个形式参数是否是引用调用
@@ -47,7 +47,7 @@ class TableRecord
 class SymbolTable
 {
   public:
-    string tableType;                    //类型，主符号表或子符号表
+    string tableType;               //类型，主符号表或子符号表
     vector<TableRecord *> records;  //指向record成员的指针
     map<string, int> idLoc;         //存储标识符在records中的下标，加快查询速度
     map<string, int> idCount;
---
+++ src/semanticAnalysis.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <set>
+#include "symbolTable.h"
+#include "ast.h"
+
+using namespace std;
+
+void symbolTableInit();
+
+void semanticAnalysis()
+{
+    symbolTableInit();
+}
