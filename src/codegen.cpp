fccaf5beab552643d156a1f5b509dfd5dd70c06b
lewisacid2021 <873227635@qq.com>
2024-04-07 05:28:21 +0000

    Update accept() method in ast.h and add visit functions to codegen.cpp

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -8,6 +8,49 @@ using std::vector;
 
 namespace ast {
 
+void GenerationVisitor::visit(AST *AST, FILE *fs)
+{
+    AST->getRoot()->accept(this, fs);
+}
+
+void GenerationVisitor::visit(AstNode *astnode, FILE *fs)
+{
+    for (auto child : astnode->getCnodeList())
+        child->accept(this, fs);
+}
+
+void GenerationVisitor::visit(LeafNode *leafnode, FILE *fs)  
+{
+    switch(leafnode->get_type()) {
+        case ConstValue::ConstvalueType::INTEGER:
+            fprintf(fs, "%d", leafnode->get_value<int>());  
+            break;
+        case ConstValue::ConstvalueType::REAL:
+            fprintf(fs, "%f", leafnode->get_value<float>()); 
+            break;
+        case ConstValue::ConstvalueType::BOOLEAN:
+            fprintf(fs, "%s", leafnode->get_value<bool>()?"true":"false");
+            break;
+        case ConstValue::ConstvalueType::CHAR:
+            fprintf(fs, "%c", leafnode->get_value<char>());
+            break;
+        case ConstValue::ConstvalueType::STRING:
+            fprintf(fs, "\"%s\"", leafnode->get_value<string>().c_str());
+            break;
+        default:
+            break;
+    }
+}
+
+void GenerationVisitor::visit(IdList *idlist, FILE *fs)
+{
+    idlist->getCnodeList()[0]->accept(this, fs);
+    if (idlist->GetGrammarType() == IdList::GrammarType::MULTIPLE_ID) {
+        fprintf(fs, ", ");
+        idlist->getCnodeList()[1]->accept(this, fs);
+  }
+}
+
 void ConstDeclaration::print_type(FILE *fs)
 {
     switch(type)
@@ -54,48 +97,27 @@ void GenerationVisitor::visit(ConstDeclaration *constdeclaration, FILE *fs)
   }
 }
 
-void GenerationVisitor::visit(IdList *idlist, FILE *fs)
-{
-    idlist->getCnodeList()[0]->accept(this, fs);
-    if (idlist->GetGrammarType() == IdList::GrammarType::MULTIPLE_ID) {
-        fprintf(fs, ", ");
-        idlist->getCnodeList()[1]->accept(this, fs);
-  }
-}
-
-void GenerationVisitor::visit(LeafNode *leafnode, FILE *fs)  
-{
-    switch(leafnode->get_type()) {
-        case ConstValue::ConstvalueType::INTEGER:
-            fprintf(fs, "%d", leafnode->get_value<int>());  
-            break;
-        case ConstValue::ConstvalueType::REAL:
-            fprintf(fs, "%f", leafnode->get_value<float>()); 
-            break;
-        case ConstValue::ConstvalueType::BOOLEAN:
-            fprintf(fs, "%s", leafnode->get_value<bool>()?"true":"false");
-            break;
-        case ConstValue::ConstvalueType::CHAR:
-            fprintf(fs, "%c", leafnode->get_value<char>());
-            break;
-        case ConstValue::ConstvalueType::STRING:
-            fprintf(fs, "\"%s\"", leafnode->get_value<string>().c_str());
-            break;
-        default:
-            break;
-    }
-}
-
-void GenerationVisitor::visit(AstNode *astnode, FILE *fs)
-{
-    for (auto child : astnode->getCnodeList())
-        child->accept(this, fs);
-}
-void GenerationVisitor::visit(AST *AST, FILE *fs)
-{
-    AST->getRoot()->accept(this, fs);
-}
 void AST::accept(Visitor *visitor, FILE *fs)
+{
+    visitor->visit(this, fs);
+}
+
+void AstNode::accept(Visitor *visitor, FILE *fs)
+{
+    visitor->visit(this, fs);
+}
+
+void LeafNode::accept(Visitor *visitor, FILE *fs)
+{
+    visitor->visit(this, fs);
+}
+
+void IdList::accept(Visitor *visitor, FILE *fs)
+{
+    visitor->visit(this, fs);
+}
+
+void ConstDeclaration::accept(Visitor *visitor, FILE *fs)
 {
     visitor->visit(this, fs);
 }
